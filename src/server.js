268670a7314190bf25926cdce4804397a6234462
Tetiana <tatiana.makarenko94@gmail.com>
2025-06-04 20:11:00 +0300

    add .gitkeep files

--- src/server.js
+++ src/server.js
@@ -21,5 +21,4 @@ export const setupServer = () => {
   app.use(errorHandler);  
   const port = Number(getEnvVar('PORT', 3000));
   app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 };
