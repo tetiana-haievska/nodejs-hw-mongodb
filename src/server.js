9bd3055e4667eeb1c5d43444df70077b03251689
Tetiana <tatiana.makarenko94@gmail.com>
2025-06-04 20:08:13 +0300

    add folders and add env

--- src/server.js
+++ src/server.js
@@ -21,4 +21,5 @@ export const setupServer = () => {
   app.use(errorHandler);  
   const port = Number(getEnvVar('PORT', 3000));
   app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 };
